613da4e711f9e0d64cf0cf121ec8728a09f7d9cb
russell <gzq8575@qq.com>
2018-06-14 18:44:14 +0800

    my works

---
+++ js/move.js
@@ -0,0 +1,37 @@
+function startMove(obj, json, fnEnd) {
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function() {
+		var bStop = true;		//假设：所有值都已经到了
+
+		for(var attr in json) {
+			var cur = 0;
+
+			if (attr == 'opacity') {
+				cur = Math.round(parseFloat($(obj).css(attr))*100);
+			} else {
+				cur = parseInt($(obj).css(attr));
+			}
+
+			var speed = (json[attr]-cur)/6;
+			speed = speed>0?Math.ceil(speed):Math.floor(speed);
+
+			if (cur != json[attr]) {
+                bStop = false;
+            }
+
+			if (attr == 'opacity') {
+				$(obj).css('filter', 'alpha(opacity:'+(cur+speed)+')');
+				$(obj).css('opacity', (cur + speed)/100);
+			} else {
+				$(obj).css(attr, cur + speed + 'px');
+			}
+		}
+
+		if (bStop) {
+			clearInterval(obj.timer);
+			if (fnEnd) {
+                fnEnd();
+            }
+		}
+	}, 30);
+}
